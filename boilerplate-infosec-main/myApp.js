8a5b65f3d6e895909580150312a580a157e5a667
Ming <54163614+mingcs@users.noreply.github.com>
2023-12-18 07:11:26 -0500

    Update myApp.js

--- boilerplate-infosec-main/myApp.js
+++ boilerplate-infosec-main/myApp.js
@@ -2,8 +2,6 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const PORT = process.env.PORT || 3030;
-
 
 
 
@@ -61,5 +59,5 @@ app.get("/", function (request, response) {
 let port = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`💡 Ming's Info Security App Started on port ${PORT}`);
+  console.log(`💡 Ming's Info Security App Started on port ${port}`);
 });
