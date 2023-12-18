6eda20ffcfde7c8e951383368c1a626e602c03ff
Ming <54163614+mingcs@users.noreply.github.com>
2023-12-18 07:10:16 -0500

    Update myApp.js

--- boilerplate-infosec-main/myApp.js
+++ boilerplate-infosec-main/myApp.js
@@ -5,9 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 
-app.listen(PORT, () => {
-  console.log(`💡 Ming's Info Security App Started on port ${PORT}`);
-});
 
 
 
@@ -62,6 +59,7 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+
+app.listen(PORT, () => {
+  console.log(`💡 Ming's Info Security App Started on port ${PORT}`);
 });
